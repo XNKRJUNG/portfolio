b4bf6ff2c919b98ddfeb0c712787ca3286dc1ff3
cyrus-xnkrjungg <shresthacyrus@outlook.com>
2024-07-17 18:06:48 -0400

    Navbar + responsive + assets

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react"
+import "./header.css"
+
+const Header = () => {
+  /*=============== Toogle Menu ===============*/
+  const [toogle, showMenu] = useState(false)
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <a href="index.html" className="nav__logo">
+          Shrestha
+        </a>
+
+        <div className={toogle ? "nav__menu show-menu" : "nav__menu"}>
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <a href="" className="nav__link active-link">
+                <i className="uil uil-estate nav__icon"></i> Home
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#home" className="nav__link">
+                <i className="uil uil-user nav__icon"></i> About
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#about" className="nav__link">
+                <i className="uil uil-file-alt nav__icon"></i> Skills
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#services" className="nav__link">
+                <i className="uil uil-briefcase-alt nav__icon"></i> Services
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#portfolio" className="nav__link">
+                <i className="uil uil-scenery nav__icon"></i> Portfolio
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#contact" className="nav__link">
+                <i className="uil uil-message nav__icon"></i> Contact
+              </a>
+            </li>
+          </ul>
+
+          <i className="uil uil-times nav__close" onClick={() => showMenu(!toogle)}></i>
+        </div>
+
+        <div className="nav__toogle" onClick={() => showMenu(!toogle)}>
+          <i className="uil uil-apps"></i>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
