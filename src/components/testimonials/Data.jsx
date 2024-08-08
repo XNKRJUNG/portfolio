9d0298c9d2c1efc44440abee67c3aa837b1fee9a
cyrus-xnkrjungg <shresthacyrus@outlook.com>
2024-08-08 16:05:41 -0400

    Component: Testimonials + Package: Swiper

---
+++ src/components/testimonials/Data.jsx
@@ -0,0 +1,24 @@
+import Image1 from "../../assets/testimonial1.png"
+import Image2 from "../../assets/testimonial2.png"
+import Image3 from "../../assets/testimonial3.png"
+
+export const Data = [
+  {
+    id: 1,
+    image: Image1,
+    title: "Tanusha Ayer",
+    description: "A really good job, all aspects of the project were followed step by steo and with good results."
+  },
+  {
+    id: 2,
+    image: Image2,
+    title: "Susil Thapa",
+    description: "A really good job, all aspects of the project were followed step by steo and with good results."
+  },
+  {
+    id: 1,
+    image: Image3,
+    title: "Raul Migel De Sauto Santos",
+    description: "A really good job, all aspects of the project were followed step by steo and with good results."
+  }
+]
